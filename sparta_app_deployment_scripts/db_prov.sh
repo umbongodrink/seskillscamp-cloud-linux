8b1497ac690de4b6295fe5ccf6aa4fa5cfb5b9fe
Kip Thompson-Born <kipthompsonborn@hotmail.com>
2024-12-18 10:50:51 +0000

    refactored db_prov.sh

--- sparta_app_deployment_scripts/db_prov.sh
+++ sparta_app_deployment_scripts/db_prov.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# install all commands in one line
+# update and upgrade the system, making sure the script is non-interactive for the user via a GUI
 sudo apt update -y && sudo DEBIAN_FRONTEND=noninteractive apt upgrade -y
 
 # import gnupg and curl
@@ -15,17 +15,13 @@ curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
 # create a list file
 echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
 
-# update the package list
+# update the package list now that the mongodb repo has been added
 sudo apt-get update
 
 ## install the mongodb, specifically version 7.0.6
-# SPECIFIC VERSION 7.0.6
-# sudo apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
-# SPECIFIC VERSION 7.0.6 but remove interactivity
 sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mongodb-org=7.0.6 mongodb-org-database=7.0.6 mongodb-org-server=7.0.6 mongodb-mongosh mongodb-org-mongos=7.0.6 mongodb-org-tools=7.0.6
 
-
-# PREVENT CERTAIN UPGRADES OF MONGODB PACKAGES WHEN RUNNING APT UPGRADE
+# prevent certain upgrades of mongodb packages when running apt upgrade in the future
 echo "mongodb-org hold" | sudo dpkg --set-selections
 echo "mongodb-org-database hold" | sudo dpkg --set-selections
 echo "mongodb-org-server hold" | sudo dpkg --set-selections
@@ -42,15 +38,15 @@ sudo systemctl enable mongod
 # backup the mongod.conf file
 sudo cp /etc/mongod.conf /etc/mongod.conf.bak
 
-# edit /etc/mongod.conf bindIp address (by default it's localhost, change to 0.0.0.0)
+# edit /etc/mongod.conf bindIp address (by default it's localhost, change to 0.0.0.0), 
+# so that the mongodb server can be accessed from any IP address
 sudo sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/' /etc/mongod.conf
 
 # restart the mongodb service
 sudo systemctl restart mongod
 
-# print the status of the mongodb service
-sudo systemctl status mongod
-
 # print the IP address of the mongodb server
 ip addr show | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}' | cut -d/ -f1
 
+# print the status of the mongodb service
+sudo systemctl status mongod
